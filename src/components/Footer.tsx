e409de2950cbb8184d4a45edb5594f64d7736255
Tamara-Ebi-Pere-Victor <tamaraebivictor@gmail.com>
2022-08-04 08:51:57 +0100

    update footer data

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,13 +4,13 @@ export const Footer = () => {
 	return (
 		<footer>
 			<a
-				href="https://github.com/Tamara-Ebi-Pere-Victor/Document-Registry"
+				href="https://github.com/Tamara-Ebi-Pere-Victor/Near-Document-Registry"
 				target={"_blank"}
 				rel="noreferrer"
 			>
 				Document Registry
 			</a>{" "}
-			- ReactJS & CELO DApp
+			- ReactJS & NEAR Protocol DApp
 		</footer>
 	)
 }
